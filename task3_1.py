a7da17b70afa0bf07b0134b0231154690a6465b9
tatiana-belova <68111007+tatiana-belova@users.noreply.github.com>
2020-07-22 13:13:34 +0300

    Delete task3_1.py

--- task3_1.py
+++
@@ -1,9 +0,0 @@
-def my_func (a, b):
-    try:
-        c = a / b
-        return c
-    except ZeroDivisionError:
-        return "На ноль делить нельзя!"
-print(my_func(int(input("Введите число а: ")), int(input("Введите число b: "))))
-
-
