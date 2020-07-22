e3f00c6f62dabf6d537e3b3c51cb4d45cc193368
tatiana-belova <68111007+tatiana-belova@users.noreply.github.com>
2020-07-22 13:13:49 +0300

    Delete task3_3.py

--- task3_3.py
+++
@@ -1,5 +0,0 @@
-def my_func(a, b, c):
-    arguments = [a, b, c]
-    arguments.remove(min(a, b, c))
-    return sum(arguments)
-print(my_func(int(input("Введите число а: ")), int(input("Введите число b: ")), (int(input("Введите число c: ")))))
