9331d22b103ba108785bd79c1c8b355f35b41fdf
tatiana-belova <68111007+tatiana-belova@users.noreply.github.com>
2020-07-24 16:40:08 +0300

    Delete task4_2.py

--- task4_2.py
+++
@@ -1,4 +0,0 @@
-my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-result = [my_list[i] for i in range(1, len(my_list)) if my_list[i] > my_list[i - 1]]
-print(f"Исходный список {my_list}")
-print(f"Результат {result}")
